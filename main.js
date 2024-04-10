334b48365667b1ba73115ca5c73a2a4aa16abc1e
Sophia <leesophia011@gmail.com>
2024-04-10 16:48:06 -0400

    delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-async function start(){
-  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/OKX/36,34/forecast")
-  const weatherData = await weatherPromise.json()
-  const ourTemperature = weatherData.properties.periods[0].temperature
-  document.querySelector("#temperature-output").textContent = ourTemperature
-}
-start()
