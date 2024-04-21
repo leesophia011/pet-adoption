6eb25e5c4329ce986e9d1d813f44e6f4e8e32677
Sophia <leesophia011@gmail.com>
2024-04-21 19:09:35 -0400

    pull in real dynamic pet data

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ async function start(){
 start()
 
 async function petsArea(){
- const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+ const petsPromise = await fetch("https://joyful-granita-840652.netlify.app/.netlify/functions/pets")
  const petsData = await petsPromise.json()
  
  petsData.forEach(pet => {
